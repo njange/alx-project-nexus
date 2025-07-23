aecf479bb338bf5bb8289374bab75c5ffd248912
njange <jamesnjange80@gmail.com>
2025-07-23 23:21:15 +0300

    feat: implement CI/CD pipeline with Docker, add API documentation, and update requirements

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Project Nexus API",
+        default_version='v1',
+        description="API documentation for ALX Project Nexus",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,  
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
